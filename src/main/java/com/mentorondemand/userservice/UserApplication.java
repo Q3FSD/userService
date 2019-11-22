b8733d7322e8ac909be6bf4b93a75f0a29fe292f
zengli <zzlili@cn.ibm.com>
2019-11-22 08:57:29 +0800

    @EnableEurekaClient

--- src/main/java/com/mentorondemand/userservice/UserApplication.java
+++ src/main/java/com/mentorondemand/userservice/UserApplication.java
@@ -6,7 +6,9 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserApplication {
 
